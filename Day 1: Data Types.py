8d36e672da13b3813e4a6f7b5ab37ba1dad221ef
sweetpanqueca <53286067+sweetpanqueca@users.noreply.github.com>
2022-07-12 18:01:26 -0300

    Create Day 1: Data Types.py

---
+++ Day 1: Data Types.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i2 = int(input())
+d2 = float(input())
+s2 = str(input())
+# Read and save an integer, double, and String to your variables.
+soma_inteiro = i + i2
+soma_decimal = d + d2
+concatenacao = s + s2
+# Print the sum of both integer variables on a new line.
+print(soma_inteiro)
+# Print the sum of the double variables on a new line.
+print(soma_decimal)
+# Concatenate and print the String variables on a new line
+print(concatenacao)
+# The 's' variable above should be printed first.
